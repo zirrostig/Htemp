a41edbc58aa1702037f160e18fad72cb18bdec2b
Zachary Stigall <zirrostig@lanfort.org>
2012-01-18 00:46:28 -0700

    Fixed a celsius conversions

--- Htemp.hs
+++ Htemp.hs
@@ -7,21 +7,21 @@ ftd = (/6) . (1060-) . (5*)
 
 --Celsius to Delisle
 ctd :: Temperature -> Temperature
-ctd = (150-) . (*(3/2))
+ctd = (150-) . ((3/2)*)
 
 --To Celsius
 --Delisle to Celsius
 dtc :: Temperature -> Temperature
-dtc :: (100-) . (*(2/3))
+dtc = (100-) . (2/3*)
 
 --Farenheit to Celsius
 ftc :: Temperature -> Temperature
-ftc = (*(5/9)) . (-32)
+ftc = (*(5/9)) . ((+) (-32))
 
 --To Farenheit
 --Celsius to Farenheit
 ctf :: Temperature -> Temperature
-ctf = (*(9/5)) . (+32)
+ctf = (+32) . (*(9/5))
 
 --Delisle to Farenheit
 dtf :: Temperature -> Temperature
